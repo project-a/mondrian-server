64be50100e6f52df1ffe995454462c750cdaa1c1
Martin <schultz-martin@web.de>
2023-08-03 13:43:18 +0200

    switch from slf4j to log4j2

--- src/main/java/mondrian/olap4j/SaikuMondrianHelper.java
+++ src/main/java/mondrian/olap4j/SaikuMondrianHelper.java
@@ -26,8 +26,10 @@ import org.olap4j.Position;
 import org.olap4j.metadata.Level;
 import org.olap4j.metadata.Measure;
 import org.olap4j.metadata.MetadataElement;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -46,7 +48,7 @@ import mondrian.rolap.*;
 public class SaikuMondrianHelper {
 
 
-    private static final Logger log = LoggerFactory.getLogger(SaikuMondrianHelper.class);
+    private static final Logger log = LogManager.getLogger(SaikuMondrianHelper.class);
 
     private static RolapConnection getMondrianConnection(OlapConnection con) {
         try {
